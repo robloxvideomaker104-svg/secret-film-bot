24506d63a92a854a24836b3982500b9f90c6a898
robloxvideomaker104-svg <robloxvideomaker104@gmail.com>
2026-09-04 20:26:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ CHANNELS_TO_SHOW = [
     {"name": "📢 1 - Zayavka Kanal", "url": "https://t.me/+T18e9PRtGvc3YTVi"},
     {"name": "📢 2 - Zayavka Kanal", "url": "https://t.me/+7WZq-XFqfV8xOGJi"},
     {"name": "📢 3 - Zayavka Kanal", "url": "https://t.me/+5WyL_f08hRUyZmRi"},
-    {"name": "📢 4 - Zayavka Kanal", "url": "https://t.me/+ldq9ICoMt8MyZTcy"},
-    {"name": "📢 5 - Zayavka Kanal", "url": "https://t.me/+-bgOOzhA7II5YWUy"},
+    {"name": "📢 4 - Zayavka Kanal", "url": "https://t.me/+CWoZivhqCC1lMDVi"},
+    {"name": "📢 5 - Zayavka Kanal", "url": "https://t.me/+zHmOMS5THK9kYzJi"},
     {"name": "⭐️ Asosiy Kanal", "url": f"https://t.me/{MAIN_CHANNEL[1:]}"}
 ]
 
